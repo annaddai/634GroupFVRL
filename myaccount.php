00370dee7bb4f6a011e2fcdd8325fcd460fdd3ef
annaddai <72436281+annaddai@users.noreply.github.com>
2023-12-01 00:25:16 -0500

    anna's part half done

---
+++ myaccount.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Account</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .card {
+            margin-bottom: 20px;
+        }
+
+        .card-deck .card {
+            flex: 1 0 auto;
+        }
+
+        .card-img-top {
+            height: 150px;
+            object-fit: cover; 
+            width: 100%;
+            border-radius: 0; 
+        }
+
+        .card-body {
+            background-color: #f8f9fa;
+        }
+
+        .card-title {
+            color: #333;
+        }
+
+        .card-text {
+            color: #666;
+        }
+
+        .card-img-profile {
+            width: 100px; /* or any other size */
+            height: 100px; /* maintain aspect ratio */
+            border-radius: 50%; /* make it round */
+            margin: 20px auto; /* centering the image */
+        }
+    </style>
+</head>
+
+
+<body>
+
+
+<div class="container mt-4">
+    <div class="row">
+            <!-- Main Content Section Placeholder - assuming it takes up 8 columns out of 12 -->
+            <div class="col-md-8">
+                <!-- Main content goes here -->  
+            </div>
+
+            <!-- User Profile Section -->
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://web.litguide.ca/client/assets/media/svg/avatars/001-boy.svg" class="card-img-profile" alt="User Avatar" >
+                    <div class="card-body text-center">
+                        <h5 class="card-title">@username_123</h5>
+                        <br>
+                        <p class="card-text"><ins>View Profile</ins></p>
+                        <p class="card-text"><ins>View Library Card</ins></p>
+                        <p class="card-text"><ins>Activity</ins></p>
+                        <p class="card-text"><ins>Settings</ins></p>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+
+
+
+<?php 
+
+$con = mysqli_connect("localhost", "root", "Daiyq0623!", "myaccount", 8889);
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
+
+function getBooks($con, $table) {
+    $result = mysqli_query($con, "SELECT * FROM $table");
+    if(!$result) {
+        echo "Error fetching books: " . mysqli_error($con);
+        return [];
+    }
+    $books = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $books;
+}
+        
+$checkedOutBooks = getBooks($con, 'checked_out');
+$holdsBooks = getBooks($con, 'holds');
+?>
+
+<div class="container mt-4">
+    <div class="row">
+        <!-- Checked Out Section -->
+        <div class="col-md-6">
+            <h3>Checked Out</h3>
+            <div class="card-deck">
+                <?php foreach ($checkedOutBooks as $book): ?>
+                    <div class="card">
+                        <img src="<?= htmlspecialchars($book['book_cover']) ?>" class="card-img-top" alt="<?= htmlspecialchars($book['book_name']) ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= htmlspecialchars($book['book_name']) ?></h5>
+                            <p class="card-text"><?= htmlspecialchars($book['author']) ?></p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div> 
+
+        <!-- On Holds Section -->
+        <div class="col-md-6">
+            <h3>On Holds</h3>
+            <div class="card-deck">
+                <?php foreach ($holdsBooks as $book): ?>
+                    <div class="card">
+                        <img src="<?= htmlspecialchars($book['book_cover']) ?>" class="card-img-top" alt="<?= htmlspecialchars($book['book_name']) ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= htmlspecialchars($book['book_name']) ?></h5>
+                            <p class="card-text"><?= htmlspecialchars($book['author']) ?></p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
