92b2c242db7fdef1cf5ff422ff9e7b355cf1578d
annaddai <72436281+annaddai@users.noreply.github.com>
2023-12-01 23:03:47 -0500

    added header

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>Fraser Valley Regional Library</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open Sans">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/header.css">
+    </head>
+<body>
+  
+  <!--Navigation Starts-->
+  <header>
+        <div class="header-container">
+            <img src="assets/img/transparentbg.png" alt="Library Logo" class="logo">
+            <nav class="navbar">
+                <a href="myaccount.php">MyFVRL</a>
+                <a href="location.php">Locations</a>
+                <a href="#booksmedia">Books & Media</a>
+                <a href="#services">Services</a>
+                <a href="#events">Events</a>
+                <a href="#help">Help</a>
+            </nav>
+            <div class="search-container">
+                <input type="text" placeholder="Search...">
+                <input type="image" src="assets/img/searchicon.png" width="35" height="35">
+            </div>
+        </div>
+    </header>
+  <!--Navigation Ends-->
+
+</body>
+</html>
--- myaccount.php
+++ myaccount.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
+
+<?php
+require 'header.php'; ?>
+
 <head>
     <meta charset="UTF-8">
     <title>My Account</title>
