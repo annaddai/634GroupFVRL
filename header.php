37dafc442d8285d628f7e2bc4752bdda7b456424
yuanxi-liu-20688855 <yxleo98@gmail.com>
2023-12-15 00:32:23 -0500

    header typo update

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
         <title>Fraser Valley Regional Library</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open Sans">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/css/header.css">
-        <link rel="stylesheet" href="assets/css/footer.css">
+        <link rel="stylesheet" href="Assets/css/header.css">
+        <link rel="stylesheet" href="Assets/css/footer.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 <style>
